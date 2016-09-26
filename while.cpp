1524734d3cef672e9484f75ad17c58cf7107b24d
Meghan <mfickett8@gmail.com>
2016-09-25 20:04:59 -0400

    while statement

---
+++ while.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n=10;
+  while (n>0) { //opens up another command to do while n is greater than 0
+    cout << "n is " <<n<<endl;
+    n--;
+  }
+  return 0;
+}
+
